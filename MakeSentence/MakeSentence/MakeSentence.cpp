aa541b3d286aa390f0d5ba08ea8d11566a393c02
Byreave Luo <654297651@qq.com>
2018-10-25 17:45:11 -0600

    add most common solution

--- MakeSentence/MakeSentence/MakeSentence.cpp
+++ MakeSentence/MakeSentence/MakeSentence.cpp
@@ -8,19 +8,46 @@
 #include <stdio.h>
 #endif // _DEBUG
 
-char * MakeSentence(const char * strings[])
+char * MakeSentence(const char strings[][128])
 {
-
+	size_t size = 0;
+	char * sentence = nullptr;
+	for (size_t i = 0; strings[i][0] != '\0'; ++i)
+	{
+		for (size_t j = 0; strings[i][j] != '\0'; ++j)
+		{
+			size++;
+		}
+		size++; // for ' '
+	}
+	size++; //for '.'
+	sentence = (char *)malloc(size);
+	size_t index = 0;
+	for (size_t i = 0; strings[i][0] != '\0'; ++i)
+	{
+		for (size_t j = 0; strings[i][j] != '\0'; ++j)
+		{
+			sentence[index++] = strings[i][j];
+		}
+		sentence[index++] = ' ';
+	}
+	sentence[index-1] = '.';
+	sentence[index] = '\0';
+	return sentence;
 }
-void main(int i_argc, char ** i_argl)
+int main(int i_argc, char ** i_argl)
 {
-	const char * strings[] = {
-		"This",
-		"is",
-		"a",
-		"test",
-		NULL
-	};
+	
+	const size_t bufferLength = 128;
+	char strings[bufferLength][bufferLength] = {};
+	unsigned int count = 0;
+
+	while (1)
+	{
+		gets_s(strings[count++], bufferLength);
+		if (strings[count - 1][0] == '\0')
+			break;
+	}
 
 	char * pSentence = MakeSentence(strings);
 
@@ -31,4 +58,5 @@ void main(int i_argc, char ** i_argl)
 #if defined(_DEBUG)
 	_CrtDumpMemoryLeaks();
 #endif // _DEBUG
+	return 0;
 }
